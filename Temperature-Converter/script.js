ba9dcebfe303243590fb59ffe1a88cbf7b42a522
Jean Staicy Auma Otambo <jeanstaicy21@gmail.com>
2024-02-15 10:10:46 +0300

    converter

---
+++ Temperature-Converter/script.js
@@ -0,0 +1,4 @@
+const celsiusField = document.querySelector("celsius");
+const degree = document.querySelector("degree");
+const convertBtn = document.querySelector("convert-btn");
+const tempType = document.querySelector("temp-type");
